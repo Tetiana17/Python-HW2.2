57695cefdf60ff330e9151ae08fbf0b9b9907b21
Tetiana17 <tetianaosiievska@gmail.com>
2024-07-04 04:21:55 -0700

    Update H.W_2_2.py

--- H.W_2_2.py
+++ H.W_2_2.py
@@ -7,5 +7,5 @@ digit_four = (number//10) % 10
 digit_five = number % 10
 
 #робимо ще одну перемінну
-reciprocal_number = (digit_five * 10000) + (digit_four * 1000) + (digit_three * 100) + (digit_two * 10) + digit_one
+reciprocal_number = digit_five * 10000 + digit_four * 1000 + digit_three * 100 + digit_two * 10 + digit_one
 print(reciprocal_number)
